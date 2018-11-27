cef3c999079a38f3fbd1e42edf3c5901bd974685
Vladimír Mužný <dropman@dropman.cz>
2018-11-27 12:38:27 +0100

    Update 04_testing_temporal_tables.sql

--- CH11_Making_Predictions/04_testing_temporal_tables.sql
+++ CH11_Making_Predictions/04_testing_temporal_tables.sql
@@ -1,5 +1,5 @@
 select Id, RecordStart from Models.ModelVersionsTemporal 
-where ModelVersionsId = 1
+where ModelTypesId = 1
 
 select Id, RecordStart from Models.ModelVersionsTemporalHistory
-where ModelVersionsId = 1
+where ModelTypesId = 1
